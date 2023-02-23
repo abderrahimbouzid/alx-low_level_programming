521ee72f84219bbaf158f7d08bf0cdbacc496614
abdo bouzid <abderrahimbouzid410@gmail.com>
2023-02-23 07:26:27 -0800

    lllove

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,7 @@
 #include <stdio.h>
-
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle\n");
+        puts("\"Programming is like building a multilingual puzzle");
     	return (0);
 }
 
