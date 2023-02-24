eccd87c7cb413774a76b37c126b26a2f50495842
abdo bouzid <abderrahimbouzid410@gmail.com>
2023-02-24 10:31:49 -0800

    bous

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,10 +9,10 @@ int main(void)
 int i, j;
 for (i = 0; i <= 8; i++)
 {
-for (j = i+1; j <= 9; j++)
+for (j = i + 1; j <= 9; j++)
 {
-putchar(i+'0');
-putchar(j+'0');
+putchar(i + '0');
+putchar(j + '0');
 if (i < 8) {
 putchar(',');
 putchar(' ');
