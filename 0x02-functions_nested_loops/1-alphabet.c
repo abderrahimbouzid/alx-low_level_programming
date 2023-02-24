60147c843fc4d7fcce482ec01ac915bde7ac00c6
abdo bouzid <abderrahimbouzid410@gmail.com>
2023-02-24 15:51:47 -0800

    loveee

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet fonction - Entry point
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+char c = 'a';
+while (c <= 'z')
+{
+_putchar(c);
+c++;
+}
+_putchar('\n');
+}
