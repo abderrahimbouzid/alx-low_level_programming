71f5c6ff95dfda3b7a59e986e75b1f6e74b8f3ef
abdo bouzid <abderrahimbouzid410@gmail.com>
2023-02-24 10:43:54 -0800

    bous

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i, j;
+for (i = 0; i <= 99; i++)
+{
+for (j = i; j <= 99; j++)
+{
+if (i != j)
+{
+putchar(i / 10 +'0');
+putchar(i % 10 +'0');
+putchar(' ');
+putchar(j / 10 +'0');
+putchar(j % 10 +'0');
+if (i < 98)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
