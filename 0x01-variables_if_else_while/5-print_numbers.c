b9fb4f6e20ef6da0cc986afbb28b19cedfafe56d
abdo bouzid <abderrahimbouzid410@gmail.com>
2023-02-24 08:15:09 -0800

    bous

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int c;
+for (c = 0; c < 10; c++)
+{
+printf("%d", c);
+}
+printf("\n");
+return (0);
+}
