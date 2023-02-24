f97f0e4cdba198c3dbb99fece3afdb0597769644
abdo bouzid <abderrahimbouzid410@gmail.com>
2023-02-24 08:08:37 -0800

    bous

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+if(c == 'e' || c == 'q')
+{
+continue;
+}
+else
+{
+putchar(c);
+}
+}
+putchar('\n');
+return (0);
+}
