c9a3b3fc0ca22b6ddfa708d5230e16aa2e8b33ba
abdo bouzid <abderrahimbouzid410@gmail.com>
2023-02-24 10:30:02 -0800

    bous

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i, j;
+for (i = 0; i <= 8; i++)
+{
+for (j = i+1; j <= 9; j++)
+{
+putchar(i+'0');
+putchar(j+'0');
+if (i < 8) {
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
