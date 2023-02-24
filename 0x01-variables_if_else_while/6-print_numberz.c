adbcda3b3599ffdf88ac432c9ba095954ffbabfb
abdo bouzid <abderrahimbouzid410@gmail.com>
2023-02-24 08:36:13 -0800

    bous

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main() {
+int i = 0;
+while (i < 10)
+{
+putchar(i + '0');
+i++;
+}
+putchar('\n');
+return (0);
+}
