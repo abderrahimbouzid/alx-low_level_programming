974921674a0094ec955b494bac8df54ae821d96c
abdo bouzid <abderrahimbouzid410@gmail.com>
2023-02-25 17:35:55 -0800

    loveee

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ */
+void jack_bauer(void)
+{
+int hours, minutes;
+for (hours = 0; hours < 24; hours++)
+{
+for (minutes = 0; minutes < 60; minutes++)
+{
+_putchar('0' + hours / 10);
+_putchar('0' + hours % 10);
+ _putchar(':');
+_putchar('0' + minutes / 10);
+_putchar('0' + minutes % 10);
+_putchar('\n');
+}
+}
+}
