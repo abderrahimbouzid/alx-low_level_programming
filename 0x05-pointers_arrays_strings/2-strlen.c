4c6fb8734cfd9a8176a140c75e9d88e8915d6757
abdo bouzid <abderrahimbouzid410@gmail.com>
2023-02-28 04:46:11 -0800

    lll

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: the string to check
+ *
+ * Return: integer.
+ */
+int _strlen(char *s)
+{
+int i = 0;
+char j = s[0];
+while (j != '\0')
+{
+j = s[i];
+i++;
+}
+return i;
+}
