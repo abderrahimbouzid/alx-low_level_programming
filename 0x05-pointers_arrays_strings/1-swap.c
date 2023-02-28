5ef4752cae6e76e2b5c8ee1be9dcf53a3babd0c9
abdo bouzid <abderrahimbouzid410@gmail.com>
2023-02-28 04:32:04 -0800

    lll

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - check the code
+ * @a: the integer to check
+ * @b: the integer to check
+ *
+ * Return: Always void.
+ */
+void swap_int(int *a, int *b)
+{
+int c;
+c = *a;
+*a = *b;
+*b = c;
+}
