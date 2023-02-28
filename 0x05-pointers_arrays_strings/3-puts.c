add4bad076fc85ec19118e6e03e33d826152f84f
abdo bouzid <abderrahimbouzid410@gmail.com>
2023-02-28 13:34:25 -0800

    lll

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -8,12 +8,11 @@
  */
 void _puts(char *str)
 {
-    int i = 0;
-
-    while (str[i] != '\0')
-    {
-        _putchar(str[i]);
-        i++;
-    }
-    _putchar('\n');
+int i = 0;
+while (str[i] != '\0')
+{
+_putchar(str[i]);
+i++;
+}
+_putchar('\n');
 }
