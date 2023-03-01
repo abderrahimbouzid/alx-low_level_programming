b492d3744e456d6ece40012103814ee9a5a83440
abdo bouzid <abderrahimbouzid410@gmail.com>
2023-03-01 06:53:41 -0800

    lll

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,7 +10,8 @@ int _atoi(char *s)
 int result = 0;
 int sign = 1;
 int i = 0;
-while (s[i] != '\0') {
+while (s[i] != '\0')
+{
 if (s[i] == '-')
 {
 sign = -sign;
@@ -18,11 +19,12 @@ sign = -sign;
 else if (s[i] >= '0' && s[i] <= '9')
 {
 result = result * 10 + (s[i] - '0');
-} else if (result > 0)
+}
+else if (result > 0)
 {
 break;
 }
 i++;
 }
-return (result * sign);
+return (result *sign);
 }
