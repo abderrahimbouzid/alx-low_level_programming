b685ccb62c2662e9104b93c76a4af51f23f5166a
abdo bouzid <abderrahimbouzid410@gmail.com>
2023-03-01 11:03:07 -0800

    loveee

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,10 +9,11 @@ int main(void)
 int i, sum = 0;
 for (i = 0; i < 1024; i++)
 {
-if (i % 3 == 0 || i % 5 == 0) {
+if (i % 3 == 0 || i % 5 == 0)
+{
 sum += i;
 }
 }
 printf("%d\n", sum);
-return 0;
+return (0);
 }
