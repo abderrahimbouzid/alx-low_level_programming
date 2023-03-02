f55e9312d2ef0f4af20b0f866dc5fc3954d828b2
abdo bouzid <abderrahimbouzid410@gmail.com>
2023-03-02 02:37:37 -0800

    mmm

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,12 +9,12 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-int i;
+int i, j;
 for (i = 0; i < n && src[i] != '\0'; i++)
 {
 dest[i] = src[i];
 }
-for ( ; i < n; i++)
+for (j = i; j < n; i++)
 {
 dest[i] = '\0';
 }
