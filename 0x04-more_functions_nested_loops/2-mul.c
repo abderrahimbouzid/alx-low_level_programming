1136a224190948dca385262f9770192921903f58
abdo bouzid <abderrahimbouzid410@gmail.com>
2023-03-02 15:14:52 -0800

    ooorig

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * function_name - Short description of what the function does
+ * @parameter1: Description of the first parameter
+ * @parameter2: Description of the second parameter
+ *
+ * Description of what the function does in more detail.
+ * Returns: Description of what the function returns
+ */
+return_type function_name(type parameter1, type parameter2)
+{
+	/* Function code goes here */
+}
