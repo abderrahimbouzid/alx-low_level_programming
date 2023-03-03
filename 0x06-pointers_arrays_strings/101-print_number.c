dc032aee3e4a432ad3242a6b6ff2f6be4311ab4e
abdo bouzid <abderrahimbouzid410@gmail.com>
2023-03-03 03:53:42 -0800

    mmm

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,21 +6,18 @@
  */
 void print_number(int n)
 {
-    int divisor = 1, digit, i;
-
-    /* Handle negative integers */
-    if (n < 0) {
-        _putchar('-');
-        n = -n;
-    }
-
-    /* Determine the divisor needed to extract the leftmost digit */
-    for (i = 0; n / divisor > 9; i++)
-        divisor *= 10;
-
-    /* Print each digit */
-    for (; divisor > 0; divisor /= 10) {
-        digit = (n / divisor) % 10;
-        _putchar(digit + '0');
-    }
+int divisor = 1, digit, i;
+if (n < 0) {
+_putchar('-');
+n = -n;
 }
+for (i = 0; n / divisor > 9; i++)
+{
+        divisor *= 10;
+}
+for (; divisor > 0; divisor /= 10) {
+digit = (n / divisor) % 10;
+_putchar(digit + '0');
+}
+}
+
