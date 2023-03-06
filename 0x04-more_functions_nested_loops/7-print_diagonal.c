c3a12d294a9370ad0b90178904ac74baba04fb0a
abdo bouzid <abderrahimbouzid410@gmail.com>
2023-03-05 17:14:36 -0800

    ooorig

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,23 +7,20 @@
  */
 void print_diagonal(int n)
 {
-	/* if n is less than or equal to 0, print only \n */
-	if (n <= 0)
-	{
-		_putchar('\n');
-	}
-	else
-	{
-		/* print a diagonal line with n \ characters */
-		for (int i = 0; i < n; i++)
-		{
-			/* print i spaces before printing the \ character */
-			for (int j = 0; j < i; j++)
-			{
-				_putchar(' ');
-			}
-			_putchar('\\');
-			_putchar('\n');
-		}
-	}
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (int i = 0; i < n; i++)
+{
+for (int j = 0; j < i; j++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
+}
 }
