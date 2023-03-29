8d9b73e96298a2416b8a0f3ce88e7a94917d182b
abdo bouzid <abderrahimbouzid410@gmail.com>
2023-03-29 08:41:49 -0700

    bous

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,6 +10,7 @@ void _puts_recursion(char *s)
 if (*s == '\0')
 {
 _putchar('\n'); /* Print a newline character */
+return;
 }
 _putchar(*s); /* Print the current character */
 _puts_recursion(s + 1); /* Recursive call with next character */
